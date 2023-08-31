31937e0203242a410e9b5625bc1bb6099f15633a
Masoud Ramezani <masoudhra@gmail.com>
2023-08-31 15:12:26 +1200

    work with string in python

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-first_name = "John"
-age = 33
-is_male = True
+# first_name = "John"
+# age = 33
+# is_male = True
+#
+#
+# print(first_name)
+# print(age)
+# print(is_male)
 
+print("Masoud \nRmezani")
+print("Masoud \tRmezani")
+print("Masoud \"Rmezani\"")
+phrase = "Masoud"
+print(phrase)
+phrase2 = phrase + " Ramezani"
+print(phrase2)
+phrase3 = phrase.lower()
+print(phrase3)
+phrase4 = phrase.upper()
+print(phrase4)
+phrase5 = phrase.replace("a", "A")
+print(phrase5)
+print(len(phrase))
+print(phrase.index("d"))
+print(phrase2.index("Ramezani"))
+print(phrase[4])
 
-print(first_name)
-print(age)
-print(is_male)
